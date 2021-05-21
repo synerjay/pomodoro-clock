e3fe7b9aa02b8173b98f94b72ecf216fd05482f1
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-21 20:25:03 +0900

    added session and break increment and decrements

--- src/container/Clock.js
+++ src/container/Clock.js
@@ -2,10 +2,38 @@ import React, { useState } from 'react';
 
 function Clock() {
   const [breakCount, setBreakCount] = useState(5);
+  // Increment Break Length
+  const incrementBreak = () => {
+    if (!isPlaying && breakCount < 60) {
+      setBreakCount(breakCount + 1);
+    }
+  };
+
+  // Decrement Break Length
+  const decrementBreak = () => {
+    if (!isPlaying && breakCount > 1) {
+      setBreakCount(breakCount - 1);
+    }
+  };
+
   const [sessionCount, setSessionCount] = useState(25);
+  //Increment Session Length
+  const incrementSession = () => {
+    if (!isPlaying && sessionCount < 60) {
+      setSessionCount(sessionCount + 1);
+    }
+  };
+
+  //Decrement SessionLength
+  const decrementSession = () => {
+    if (!isPlaying && sessionCount > 1) {
+      setSessionCount(sessionCount - 1);
+    }
+  };
+
+  const [isPlaying, setIsPlaying] = useState(false);
   const [clockCount, setClockCount] = useState(25 * 60);
   const [currentTimer, setCurrentTimer] = useState('Session');
-  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div>
