467d17e0b3983fff32d72d343a2f7ce18fe7fd79
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-22 19:00:59 +0900

    Added buttons section

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,7 +4,7 @@ function Buttons({ reset, start, stop, isPlaying }) {
   return (
     <div className='buttons'>
       <button id='start_stop' onClick={isPlaying ? stop : start}>
-        {isPlaying ? <h2> Stop</h2> : <h2> Play</h2>}
+        {isPlaying ? <p> Stop</p> : <p> Play</p>}
       </button>
       <button id='reset' onClick={reset}>
         Reset
