186efa4ea14f73ef0ffc6c18e6618cf93614ab2e
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-23 13:01:31 +0900

    added session styling

--- src/components/Session.js
+++ src/components/Session.js
@@ -3,22 +3,22 @@ import React from 'react';
 function Session({ increment, decrement, sessionCount }) {
   return (
     <div className='SessionTimer'>
-      <h2 id='session-label'>Session Length:</h2>{' '}
+      <h3 id='session-label'>Session Length:</h3>{' '}
       <div className='SessionButtons'>
-        <button
-          className='incrementWork'
-          id='session-increment'
-          onClick={increment}
-        >
-          +
-        </button>
-        <h2 id='session-length'>{sessionCount}</h2>
         <button
           className='decrementWork'
           id='session-decrement'
           onClick={decrement}
         >
           -
+        </button>
+        <h3 id='session-length'>{sessionCount}</h3>
+        <button
+          className='incrementWork'
+          id='session-increment'
+          onClick={increment}
+        >
+          +
         </button>
       </div>
     </div>
